799f1c3424daf83ab0829e34feed00af71b050ec
Abdullah Moubayed <abdullah.moubayed@outlook.com>
2021-10-28 07:12:31 +0200

    Start

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        språk: [
+            { url: 'assets/img/språk/html.png', title: 'HTML' },
+            { url: 'assets/img/språk/css.png', title: 'CSS' },
+            { url: 'assets/img/språk/javascript.png', title: 'Javascript' },
+            { url: 'assets/img/språk/sass.png', title: 'SCSS' },
+            { url: 'assets/img/språk/php.png', title: 'php' },
+            { url: 'assets/img/språk/c.png', title: 'C#' },
+            { url: 'assets/img/språk/3d.png', title: '3D' },
+            { url: 'assets/img/språk/xd.png', title: 'xd' },
+            { url: 'assets/img/språk/unity.png', title: 'unity' },
+            { url: 'assets/img/språk/vr.png', title: 'VR' },
+            
+        ]
+      }
+    }
+  })
+  
+  app.mount('#app')
+  
