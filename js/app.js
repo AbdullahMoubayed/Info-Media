e3886cb946ca2802a338f8b5e1df5f1b5869fc38
SaxManDS <david.skillborg@edu.alleskolan.eu>
2022-01-29 17:40:03 +0100

    massa skit

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ const app = Vue.createApp({
         { url: 'assets/img/språk/sass.png', title: 'SCSS' },
         { url: 'assets/img/språk/php.png', title: 'PHP' },
         { url: 'assets/img/språk/c.png', title: 'C#' },
-        { url: 'assets/img/språk/3d.png', title: '3D' },
+        { url: 'assets/img/språk/3d.png', title: 'Blender' },
         { url: 'assets/img/språk/xd.png', title: 'XD' },
         { url: 'assets/img/språk/unity.png', title: 'Unity' },
         { url: 'assets/img/språk/vr.png', title: 'VR' },
