e3424abdc9508bc3f5e4749443c06656ccde48f7
SaxManDS <david.skillborg@edu.alleskolan.eu>
2022-02-21 16:24:37 +0100

    bluuuh

--- js/app.js
+++ js/app.js
@@ -16,12 +16,12 @@ const app = Vue.createApp({
       språk: [
         { url: 'assets/img/språk/html.png', title: 'HTML' },
         { url: 'assets/img/språk/css.png', title: 'CSS' },
-        { url: 'assets/img/språk/javascript.png', title: 'Javascript' },
+        { url: 'assets/img/språk/javascript.png', title: 'JavaScript' },
         { url: 'assets/img/språk/sass.png', title: 'SCSS' },
         { url: 'assets/img/språk/php.png', title: 'PHP' },
         { url: 'assets/img/språk/c.png', title: 'C#' },
         { url: 'assets/img/språk/3d.png', title: 'Blender' },
-        { url: 'assets/img/språk/xd.png', title: 'XD' },
+        { url: 'assets/img/språk/xd.png', title: 'Adobe XD' },
         { url: 'assets/img/språk/unity.png', title: 'Unity' },
         { url: 'assets/img/språk/vr.png', title: 'VR' },
 
